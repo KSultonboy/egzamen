3db7e4401b96ca8906ac3488e749eadaefca467d
verve-neowise <komiljonovsultonboy954@gmail.com>
2023-08-01 18:25:33 +0500

    first commit

---
+++ 3-bosqich/accardion/app.js
@@ -0,0 +1,7 @@
+const hotel = document.querySelector(".hotel");
+const beach = document.querySelector(".beach");
+const here = document.querySelector(".here");
+const hotelComment = document.querySelector(".hotel_comment");
+hotel.addEventListener("click", function () {
+  hotel.classList.remove(".close");
+});
